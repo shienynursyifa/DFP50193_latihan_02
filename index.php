3c0cd97b4c3097837b1da65317dbc7e2ce9a83a9
shienynursyifa <shienynursyifa@gmail.com>
2021-09-30 10:51:37 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ms">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Kedua</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+
+  <?php
+  $nama = 'Shieny';
+  $umur = 15 + 5 ;
+
+  echo "$nama berumur $umur tahun ";
+
+  ?>
+
+
+<!-- Shieny berumur 20 tahun-->
+    
+</body>
+</html>
